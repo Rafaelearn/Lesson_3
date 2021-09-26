553daaf126341759d2164f0492f554d4bb776ccb
Рафаэль Халилулин <RFKhalilulin@stud.kpfu.ru>
2021-09-26 12:53:36 +0300

    My first solution. Used type TimeDate

--- Lab_3/Laboratory.cs
+++ Lab_3/Laboratory.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lab_3
 {
+    enum Mounth
+    {
+        January = 1,
+        February,
+        March,
+        April, 
+        May,
+        June,
+        July,
+        August,
+        September,
+        Ocober,
+        November,
+        December
+    }
     class Laboratory
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("All laboratory work");
+            bool flag = true;
+            var date = new DateTime(1, 12, 31);
+            ushort day;
+            ushort year;
+            Console.Write("Введите порядковый номер дня в текущем году n(1 <= n <= 365): ");
+            while (flag)
+            {
+                ushort.TryParse(Console.ReadLine(), out day);
+                if (day<1 || day>365)
+                {
+                    Console.Write("n не принадлежит указанному промежутку. Попробуй снова: ");
+                }
+                else
+                {
+                    Console.Write("Введите год: ");
+                    ushort.TryParse(Console.ReadLine(), out year);
+                    date = date.AddYears(year-1);
+                    date = date.AddDays(day);
+                    Console.WriteLine($"Current date: {date.Day} {(Mounth)date.Month}");
+                    flag = false;
+                }
+
+            }
+
             Console.ReadKey();
         }
     }
