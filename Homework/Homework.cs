42344d1d290d7dc565d5a1291df07997cee32a1b
Рафаэль Халилулин <RFKhalilulin@stud.kpfu.ru>
2021-09-27 19:37:45 +0300

    Fished labaratory. 1 Version. Added enum struct. Used if-else*12. May be I will found little better solution.

--- Homework/Homework.cs
+++ Homework/Homework.cs
@@ -10,6 +10,28 @@ namespace Homework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Task 1.1");
+            Console.Write("Введите действительное число x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Его абсолютная величина равна {Abs(x)}");
+
+            Console.WriteLine("\nTask 1.2");
+            Console.Write("Введите действительное число x = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите действительное число x = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            x = Abs(x) > Abs(y) ? x / 2 : x;
+            Console.WriteLine($"После преобразования x = {x}");
+
+            Console.WriteLine("\nTask 1.3");
+            string s = Console.ReadLine();
+
+            
+        }
+        static double Abs(double x)
+        {
+            x = x > 0 ? x : -x;
+            return x;
         }
     }
 }
--- Lab_3/Laboratory.cs
+++ Lab_3/Laboratory.cs
@@ -21,25 +21,41 @@ namespace Lab_3
         static void Main(string[] args)
         {
             Console.WriteLine("All laboratory work");
-            bool flag = true;
-            var date = new DateTime(1, 12, 31);
-            ushort day;
-            ushort year;
-            Console.Write("Введите порядковый номер дня в текущем году n(1 <= n <= 365): ");
+            Console.Write("Введите год: ");
+            ushort dayAdd = Convert.ToUInt16(Console.ReadLine());
+            dayAdd =  (ushort)(dayAdd % 400 == 0 || dayAdd % 4 == 0 && !(dayAdd % 100 == 0) ? 1 : 0);
+            if(dayAdd == 1)
+            {
+                Console.WriteLine("Введенный год високосный");
+            }
+            else
+            {
+                Console.WriteLine("Введенный год невисокосный");
+            }
+            Console.Write("Введите порядковый номер дня в текущем году: ");
+            ushort day; bool flag = true;
             while (flag)
             {
                 ushort.TryParse(Console.ReadLine(), out day);
-                if (day<1 || day>365)
+                if (day < 1 || day > 365 + dayAdd)
                 {
-                    Console.Write("n не принадлежит указанному промежутку. Попробуй снова: ");
+                    Console.Write("n не может принимать данное значение. Попробуй снова: ");
                 }
                 else
                 {
-                    Console.Write("Введите год: ");
-                    ushort.TryParse(Console.ReadLine(), out year);
-                    date = date.AddYears(year-1);
-                    date = date.AddDays(day);
-                    Console.WriteLine($"Current date: {date.Day} {(Mounth)date.Month}");
+                    Console.WriteLine("Current date: ");
+                    if (day <= 31) { Console.Write($"{day} { (Mounth)1}"); }
+                    else if (day <= 59 + dayAdd) { Console.Write($"{day - 31} { (Mounth)2}"); }
+                    else if (day <= 90 + dayAdd) {Console.Write($"{day- 59 -dayAdd} { (Mounth)3}"); }
+                    else if (day <= 120 + dayAdd) { Console.Write($"{day - 90 - dayAdd} { (Mounth)4}"); }
+                    else if (day <= 151 + dayAdd) { Console.Write($"{day - 120 - dayAdd} { (Mounth)5}"); }
+                    else if (day <= 181 + dayAdd) { Console.Write($"{day - 121 - dayAdd} { (Mounth)6}"); }
+                    else if (day <= 212 + dayAdd) { Console.Write($"{day - 181 - dayAdd} { (Mounth)7}"); }
+                    else if (day <= 243 + dayAdd) { Console.Write($"{day - 212 - dayAdd} { (Mounth)8}"); }
+                    else if (day <= 273 + dayAdd) { Console.Write($"{day - 243 - dayAdd} { (Mounth)9}"); }
+                    else if (day <= 304 + dayAdd) { Console.Write($"{day - 273 - dayAdd} { (Mounth)10}"); }
+                    else if (day <= 334 + dayAdd) { Console.Write($"{day - 304 - dayAdd} { (Mounth)11}"); }
+                    else if (day <= 365 + dayAdd) { Console.Write($"{day - 334 - dayAdd} { (Mounth)12}"); }
                     flag = false;
                 }
 
